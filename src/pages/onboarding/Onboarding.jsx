55015abee343158229ecb0aa0bffd8d5866e9586
Jihyun <hjh20030615@gmail.com>
2025-12-20 02:28:00 +0900

    feat: 온보딩 페이지 UI 제작 완료

--- src/pages/onboarding/Onboarding.jsx
+++ src/pages/onboarding/Onboarding.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import Mascot from '../../assets/img/img_mascot.svg';
+import Logo from '../../assets/img/ic_logo.svg';
 
 const Onboarding = () => {
     const navigate = useNavigate();
@@ -10,7 +12,14 @@ const Onboarding = () => {
 
     return (
         <div className="Onboarding_wrap">
-            온보딩 페이지입니다.
+            <div className="subtitle">관계의 온도를 맞추는</div>
+            <div className="title">센스있는 사회생활 가이드</div>
+            <div className="logo">
+                <img src={Logo} alt="Logo" />
+            </div>
+            <div className="mascot">
+                <img src={Mascot} alt="Mascot" />
+            </div>
             <button onClick={handleStart}>시작하기</button>
         </div>
     )
